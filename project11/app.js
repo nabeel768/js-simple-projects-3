9f20da3239c0b721b06015facdef3eb20c3441f7
Nabeel Khan <nabeel.kay@gmail.com>
2023-02-26 16:29:55 +0000

    created template

---
+++ project11/app.js
@@ -0,0 +1,49 @@
+var character = document.querySelector('#character');
+var game = document.querySelector('#game');
+var interval;
+var both = 0;
+var counter = 0;
+
+function moveLeft() {
+  var left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+  if (left > 0) {
+    character.style.left = left - 2 + 'px';
+  }
+}
+function moveRight() {
+  var left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+  if (left < 380) {
+    character.style.left = left + 2 + 'px';
+  }
+}
+
+document.addEventListener('keydown', (event) => {
+  if (both == 0) {
+    both++;
+    if (event.key === 'ArrowLeft') {
+      interval = setInterval(moveLeft, 1);
+    }
+    if (event.key === 'ArrowRight') {
+      interval = setInterval(moveRight, 1);
+    }
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  clearInterval(interval);
+  both = 0;
+});
+
+setInterval(function () {
+  var block = document.createElement('div');
+  var hole = document.createElement('div');
+  block.setAttribute('class', 'block');
+  hole.setAttribute('class', 'hole');
+  block.setAttribute('id', 'block' + counter);
+  hole.setAttribute('id', 'hole' + counter);
+  var random = Math.floor(Math.random() * 360);
+  hole.style.left = random + 'px';
+  game.appendChild(block);
+  game.appendChild(hole);
+  counter++;
+}, 1);
