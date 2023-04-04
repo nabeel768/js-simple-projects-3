8d8922878a967d655f943a881fd33aee500404d5
Nabeel Khan <nabeel.kay@gmail.com>
2023-04-04 23:52:17 +0100

    finished snake game

--- project12/app.js
+++ project12/app.js
@@ -145,4 +145,16 @@ function displayGameOver() {
   ctx.fillText("GAME OVER!", gameWidth/2, gameHeight/2)
   running=false
 }
-function resetGame() {}
+function resetGame(){
+  score= 0
+  xVelocity=unitSize
+  yVelocity=0
+  snake = [
+    { x: unitSize * 4, y: 0 },
+    { x: unitSize * 3, y: 0 },
+    { x: unitSize * 2, y: 0 },
+    { x: unitSize, y: 0 },
+    { x: 0, y: 0 },
+  ];
+  gameStart()
+}
